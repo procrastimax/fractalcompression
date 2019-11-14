daf9c49287bc8ccdb8c1ce8bad3097fce5e5e0aa
procrastimax <heykeroth.dev@gmail.com>
2019-11-14 16:53:56 +0100

    Add function to calculate average gray level of an image
    Add function to calculate average gray level of an image

--- imagetools/imagecolortools.go
+++ imagetools/imagecolortools.go
@@ -39,3 +39,15 @@ func ImageToBW(img *image.Image) *image.Gray {
 	}
 	return newImg
 }
+
+//calcAverageGrayLevel iterates over all pixel in the image and calcuates the average gray level
+func calcAverageGrayLevel(img *image.Gray) uint8 {
+	var grayValue int = 0
+	var x, y = 0, 0
+	for i := range img.Pix {
+		x = i % img.Stride
+		y = i / img.Stride
+		grayValue += int(img.GrayAt(x, y).Y)
+	}
+	return uint8(grayValue / len(img.Pix))
+}
--- main.go
+++ main.go
@@ -22,19 +22,21 @@ func main() {
 
 	fmt.Println("Image successfully turned gray...")
 
-	fmt.Println("Creating range and domain arrays...")
-	//ranges := imagetools.DivideImage(grayImg, 4)
-	domains := imagetools.DivideImage(grayImg, 8)
+	grayImg = imagetools.CreateFractalFromImage(grayImg, 1, []imagetools.Transformation{transformation1, transformation2, transformation3})
 
-	fmt.Println("Scale down all domains to range-block size...")
+	//fmt.Println("Creating range and domain arrays...")
+	//ranges := imagetools.DivideImage(grayImg, 4)
+	//domains := imagetools.DivideImage(grayImg, 8)
+
+	/*fmt.Println("Scale down all domains to range-block size...")
 	for i := range domains {
 		for _, value := range domains[i] {
 			value = imagetools.ScaleImage(value, 0.5)
 		}
-	}
+	}*/
 
-	//imagetools.SaveImageToFile(grayImg, filename)
-	//fmt.Println("Image successfully saved...")
+	imagetools.SaveImageToFile(grayImg, filename)
+	fmt.Println("Image successfully saved...")
 }
 
 //Sierpinski Triangle
